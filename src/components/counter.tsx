49c2abf671a58929686fb71d1a6a70e13a47d7d3
7on-ai <team7on.ai@gmail.com>
2025-07-09 14:11:32 +0700

    Update counter.tsx

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -22,8 +22,8 @@ export function Counter() {
         if (typeof data.count !== "number") {
           throw new Error("Invalid count received from API");
         }
-        // เพิ่มค่าเริ่มต้น 1176
-        setCount(data.count + 1176);
+        // เพิ่มค่าเริ่มต้น 1178
+        setCount(data.count + 1178);
       } catch (err) {
         console.error("Error fetching waitlist count:", err);
         setError(
